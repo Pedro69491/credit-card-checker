c9000288b7072b01cde02201e2c9072843016177
Pedro69491 <48108176+Pedro69491@users.noreply.github.com>
2020-07-04 19:16:54 +0100

    Add files via upload

---
+++ credit card checker.js
@@ -0,0 +1,106 @@
+// All valid credit card numbers
+const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8]
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5]
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6]
+
+// All invalid credit card numbers
+const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5]
+const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3]
+const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4]
+const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5]
+const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4]
+
+// Can be either valid or invalid
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4]
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9]
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3]
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
+
+// An array of all the arrays above
+const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
+
+
+
+// Algorithm used to check if the cards are valide
+const luhnAlgorithm = (num) => {
+    let totalValue = 0;
+    let number = num * 2;
+    if (number > 9) {
+        totalValue += number - 9;
+        return totalValue;
+    }
+    else {
+        totalValue += number;
+        return totalValue;
+    }
+}
+
+// function that is going to return a boolean value in order to determine whether a certain card is valid or not
+const validateCred = (arr) => {
+    let totalValue = 0;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr.length%2 === 0) {
+            if (i%2 === 0) {
+                totalValue += luhnAlgorithm(arr[i]);
+            }
+            else {
+                totalValue += arr[i];
+            }
+        }
+        else {
+            if (i%2 !== 0) {
+                totalValue += luhnAlgorithm(arr[i]);
+            }
+            else {
+                totalValue += arr[i];
+                }
+        }
+    }
+    if (totalValue%10 === 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+};
+
+//  returning of a list of several invalid cards
+const findInvalidCards = (arr) => {
+    const newList = arr.filter(n => {
+        return validateCred(n) !== 'Valid card';
+    })
+    return newList;
+}
+
+// Function which traces the origin of the invalid cards and returns a list of company's names
+const IdinvalidCardCompanies = (arr) => {     
+    const invalidCompanies = arr.map(element => {
+        if (element[0] === 3) {
+            return 'American Express';
+        }
+        else if (element[0] === 4) {
+            return "Visa";
+        }
+        else if (element[0] === 5) {
+            return "MasterCard";
+        }
+        else if (element[0] === 6) {
+            return "Discover";
+        }
+        else {
+            console.log('Company not found')
+        }
+    })
+    const uniqueValues = invalidCompanies.filter((x,i,a) => a.indexOf(x) === i);
+    return uniqueValues;
+}
+
+console.log(validateCred(mystery1))
+console.log(validateCred(mystery2))
+console.log(validateCred(mystery3))
+console.log(validateCred(mystery4))
+console.log(validateCred(mystery5))
+console.log(IdinvalidCardCompanies(findInvalidCards(batch)))
